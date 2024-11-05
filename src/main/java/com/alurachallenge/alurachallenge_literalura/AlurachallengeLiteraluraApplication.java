d12c6a4b13a6b95fedd8535340a0dd44edb3ed3a
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-05 10:49:23 -0500

    Spring Initializr

---
+++ src/main/java/com/alurachallenge/alurachallenge_literalura/AlurachallengeLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.alurachallenge_literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlurachallengeLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlurachallengeLiteraluraApplication.class, args);
+	}
+
+}
