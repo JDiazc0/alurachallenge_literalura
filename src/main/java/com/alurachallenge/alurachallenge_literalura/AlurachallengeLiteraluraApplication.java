570f21bb06717e8563356d0d3e1a8d6de02cbe25
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-13 23:39:00 -0500

    feat main application

--- src/main/java/com/alurachallenge/alurachallenge_literalura/AlurachallengeLiteraluraApplication.java
+++ src/main/java/com/alurachallenge/alurachallenge_literalura/AlurachallengeLiteraluraApplication.java
@@ -1,13 +1,29 @@
 package com.alurachallenge.alurachallenge_literalura;
 
+import com.alurachallenge.alurachallenge_literalura.MainMenu.MainMenu;
+import com.alurachallenge.alurachallenge_literalura.Repository.AuthorRepository;
+import com.alurachallenge.alurachallenge_literalura.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlurachallengeLiteraluraApplication {
+public class AlurachallengeLiteraluraApplication implements CommandLineRunner {
+
+	private final MainMenu mainMenu;
+
+	@Autowired
+	public AlurachallengeLiteraluraApplication(MainMenu mainMenu) {
+		this.mainMenu = mainMenu;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlurachallengeLiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		mainMenu.showMenu();
+	}
 }
