352e1613aaac0c60d1f87b4a1d3795e0e7e22cd1
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-13 23:35:56 -0500

    refactor language

--- src/main/java/com/alurachallenge/alurachallenge_literalura/Models/Language.java
+++ src/main/java/com/alurachallenge/alurachallenge_literalura/Models/Language.java
@@ -1,18 +1,15 @@
 package com.alurachallenge.alurachallenge_literalura.Models;
 
 public enum Language {
-    ES("es","Spanish", "Español"),
-    EN("en","English","Inglés"),
-    PT("pt", "Portuguese","Portugués"),
-    FR("fr", "French","Francés");
+    ES("es"),
+    EN("en"),
+    PT("pt"),
+    FR("fr"),
+    DE("de");
 
     private String languageCode;
-    private String languageEnglish;
-    private String languageSpanish;
-    Language(String languageCode, String languageEnglish, String languageSpanish){
+    Language(String languageCode){
         this.languageCode = languageCode;
-        this.languageEnglish = languageEnglish;
-        this.languageSpanish = languageSpanish;
     }
 
     public static Language fromCode(String text){
@@ -22,23 +19,5 @@ public enum Language {
             }
         }
         throw new IllegalArgumentException("No encontrado para el código "+text);
-    }
-
-    public static Language fromEnglish(String text){
-        for (Language language: Language.values()){
-            if (language.languageEnglish.equalsIgnoreCase(text)){
-                return language;
-            }
-        }
-        throw new IllegalArgumentException("No encontrado para el idioma "+text);
-    }
-
-    public static Language fromSpanish(String text){
-        for (Language language: Language.values()){
-            if (language.languageSpanish.equalsIgnoreCase(text)){
-                return language;
-            }
-        }
-        throw new IllegalArgumentException("No encontrado para el idioma "+text);
     }
 }
